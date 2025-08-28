b89d094b051e033b9cb1437f83e5a6f97d416731
danereid1024 <danereidcode@gmail.com>
2025-08-27 23:37:14 -0400

    finally fixed cheetah's tail

--- js/plugins/items/Cheetah'sTail.js
+++ js/plugins/items/Cheetah'sTail.js
@@ -1,21 +1,27 @@
+const equipSlot1 = $gameParty.leader().equips()[0]?.id;
+const equipSlot2 = $gameParty.leader().equips()[1]?.id;
+const pageup = Input.isPressed("pageup");
+const pagedown = Input.isPressed("pagedown");
+
 $gameMessage.setChoices(["Normal", "Fast", "Very fast", "Nevermind"], 0, 3);
 $gameMessage.setChoiceBackground(1);
 $gameMessage.setChoicePositionType(1);
-$gameMessage.setChoiceCallBack(n => {
-    $gameVariables.setValue(5, n);
+$gameMessage.setChoiceCallBack((n) => {
+  $gameVariables.setValue(5, n);
 });
 this.setWaitMode("message");
 
-switch($gameVariables.value(1)) {
-    case 0:
-        $gamePlayer.setMoveSpeed(3);
-        break;
-    case 2:
-        $gamePlayer.setMoveSpeed(4);
-        break;
-    case 3:
-        $gamePlayer.setMoveSpeed(5);
-        break;
-    case 4:
-        break;
+switch ($gameVariables.value(5)) {
+  case 0:
+    $gamePlayer.setMoveSpeed(4);
+    break;
+  case 1:
+    $gamePlayer.setMoveSpeed(5);
+    break;
+  case 2:
+    $gamePlayer.setMoveSpeed(6);
+    break;
+    default:
+      $gamePlayer.setMoveSpeed(4);
+    break;
 }
